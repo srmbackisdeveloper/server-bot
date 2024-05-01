7ae7cec0f657a0c02312288e71fd9517c998cd60
Syrymbek Zhumazhan <75174477+srmbackisdeveloper@users.noreply.github.com>
2024-05-01 23:50:54 +0600

    Minor changes

--- internal/server/server.go
+++ internal/server/server.go
@@ -37,7 +37,7 @@ func NewServer() *http.Server {
 	}
 
 	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", NewServer.port),
+		Addr:         fmt.Sprintf("0.0.0.0:%d", port),
 		Handler:      NewServer.RegisterRoutes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
